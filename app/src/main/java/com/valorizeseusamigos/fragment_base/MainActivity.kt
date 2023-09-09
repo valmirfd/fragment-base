ef7b68f994f603a26c2865878132e0f04a397af7
valmirfd <fernandocursos2021@gmail.com>
2023-09-09 20:51:06 -0300

    Alterando o fragmentConteudo para o topo para que ele seja exibido

--- app/src/main/java/com/valorizeseusamigos/fragment_base/MainActivity.kt
+++ app/src/main/java/com/valorizeseusamigos/fragment_base/MainActivity.kt
@@ -2,10 +2,20 @@ package com.valorizeseusamigos.fragment_base
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.valorizeseusamigos.fragment_base.fragments.ConversasFragment
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val fragmentManager = supportFragmentManager.beginTransaction()
+
+        //alterações em fragments
+        fragmentManager.add(R.id.fragmentConteudo, ConversasFragment())
+
+
+        fragmentManager.commit()
+
     }
 }
